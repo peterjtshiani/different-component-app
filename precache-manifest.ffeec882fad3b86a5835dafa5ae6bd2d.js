75c9ff3fd0dc15a515e5a70b1a4d7e3c6481c584
Peter Tshiani <tshianipeter@gmail.com>
2019-10-16 13:54:49 +0200

    Updates

---
+++ precache-manifest.ffeec882fad3b86a5835dafa5ae6bd2d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e6491d73016080faca1078997fbf09",
+    "url": "/different-component-app/index.html"
+  },
+  {
+    "revision": "83a46cf4d57086820797",
+    "url": "/different-component-app/static/css/main.fd730fd9.chunk.css"
+  },
+  {
+    "revision": "9bb0636fa6c19cd58ccf",
+    "url": "/different-component-app/static/js/2.4c9798d9.chunk.js"
+  },
+  {
+    "revision": "83a46cf4d57086820797",
+    "url": "/different-component-app/static/js/main.01953a63.chunk.js"
+  },
+  {
+    "revision": "f4c6baf670e6709c0cb7",
+    "url": "/different-component-app/static/js/runtime~main.f3035c03.js"
+  },
+  {
+    "revision": "497c2d6697eda1282bd50f2b7f544e8f",
+    "url": "/different-component-app/static/media/2.497c2d66.jpeg"
+  },
+  {
+    "revision": "6d04501023df432ab2b2fdfc121a0819",
+    "url": "/different-component-app/static/media/logo.6d045010.jpg"
+  },
+  {
+    "revision": "982dffaa0051966698b78eacf3631a09",
+    "url": "/different-component-app/static/media/logo1.982dffaa.png"
+  },
+  {
+    "revision": "93dab6b0b5282175ac14cbec504230e1",
+    "url": "/different-component-app/static/media/mb.93dab6b0.jpg"
+  },
+  {
+    "revision": "0b7c1d8df10c764b42cfedbc8cf98484",
+    "url": "/different-component-app/static/media/promotion2019.0b7c1d8d.jpg"
+  }
+]);
